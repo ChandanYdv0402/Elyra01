e228bd7127395e91341563cd59bc35aeb44b6bdb
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 02:25:15 +0530

    feat(data): integrate getWebinarByPresenterId and pass webinarStatus param

--- elyra/src/app/(protectedRoutes)/webinars/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/page.tsx
@@ -1,13 +1,20 @@
 "use client";
  import React from "react";
-+import { onAuthenticateUser } from "@/action/auth";
-+import { redirect }           from "next/navigation";
+ import { onAuthenticateUser } from "@/action/auth";
+ import { redirect }           from "next/navigation";
++import { getWebinarByPresenterId } from "@/actions/webinar";
++import { WebinarStatusEnum }       from "@prisma/client";
 
  const page = async ({ searchParams }) => {
-+  const checkUser = await onAuthenticateUser();
-+  if (!checkUser.user) {
-+    redirect("/");
-+  }
++  const { webinarStatus } = await searchParams;
+   const checkUser = await onAuthenticateUser();
+   if (!checkUser.user) {
+     redirect("/");
+   }
++  const webinars = await getWebinarByPresenterId(
++    checkUser.user.id,
++    webinarStatus as WebinarStatusEnum
++  );
 
    return <div>Loading...</div>;
  };

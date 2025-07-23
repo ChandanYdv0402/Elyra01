ee8e621de62e2c23ba91cc1c4fcac7d52010a6d5
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 02:23:46 +0530

    feat(auth): add onAuthenticateUser guard with redirect to “/”

--- elyra/src/app/(protectedRoutes)/webinars/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/page.tsx
@@ -1,8 +1,13 @@
 "use client";
-import React from "react";
+ import React from "react";
++import { onAuthenticateUser } from "@/action/auth";
++import { redirect }           from "next/navigation";
 
-const page = async ({ searchParams }) => {
-  return <div>Loading...</div>;
-};
+ const page = async ({ searchParams }) => {
++  const checkUser = await onAuthenticateUser();
++  if (!checkUser.user) {
++    redirect("/");
++  }
 
-export default page;
+   return <div>Loading...</div>;
+ };

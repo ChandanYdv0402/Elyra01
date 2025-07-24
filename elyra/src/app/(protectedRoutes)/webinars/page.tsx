bea7c2400b0c0f9d334593f38b355ee5bf1412dd
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 14:49:26 +0530

    feat(ui): add Tabs, TabsList & TabsTrigger for status filtering and pushed prisma schema file

--- elyra/src/app/(protectedRoutes)/webinars/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/page.tsx
@@ -2,19 +2,20 @@
  import React from "react";
  import { onAuthenticateUser } from "@/action/auth";
  import { redirect }           from "next/navigation";
-+import { getWebinarByPresenterId } from "@/actions/webinar";
-+import { WebinarStatusEnum }       from "@prisma/client";
+ import { getWebinarByPresenterId } from "@/actions/webinar";
+ import { WebinarStatusEnum }       from "@prisma/client";
++import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
  const page = async ({ searchParams }) => {
-+  const { webinarStatus } = await searchParams;
-   const checkUser = await onAuthenticateUser();
-   if (!checkUser.user) {
-     redirect("/");
-   }
-+  const webinars = await getWebinarByPresenterId(
-+    checkUser.user.id,
-+    webinarStatus as WebinarStatusEnum
+   …  
+-  return <div>Loading...</div>;
++  return (
++    <Tabs defaultValue="all" className="w-full">
++      <TabsList>
++        <TabsTrigger value="all">All</TabsTrigger>
++        <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
++        <TabsTrigger value="ended">Ended</TabsTrigger>
++      </TabsList>
++    </Tabs>
 +  );
-
-   return <div>Loading...</div>;
  };

4994f8090893dd9aee6c4886599f1d22a38b22a4
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 22:53:31 +0530

    feat(auth): restrict access to presenter only

--- elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
@@ -22,6 +22,48 @@ const page = async ({ params }: Props) => {
       </div>
     );
   }
+"use client";
+
+import React from "react";
+import { onAuthenticateUser }   from "@/action/auth";
+import { redirect }             from "next/navigation";
+import { getWebinarAttendance } from "@/action/attendance";
+
+type Props = {
+  params: Promise<{
+    webinarId: string;
+  }>;
+};
+
+const page = async ({ params }: Props) => {
+  const { webinarId } = await params;
+  const pipelineData = await getWebinarAttendance(webinarId);
+
+  if (!pipelineData.data) {
+    return (
+      <div className="text-3xl h-[400px] flex justify-center items-center">
+        No Pipelines Found
+      </div>
+    );
+  }
+
+  const checkUser = await onAuthenticateUser();
+  if (!checkUser.user) {
+    redirect("/sign-in");
+  }
+
+  if (checkUser.user.id !== pipelineData.presenter.id) {
+    return (
+      <div className="text-3xl h-[400px] flex justify-center items-center">
+        You are not authorized to view this page
+      </div>
+    );
+  }
+
+  return <div>Loading pipeline attendance...</div>;
+};
+
+export default page;
 
   const checkUser = await onAuthenticateUser();
   if (!checkUser.user) {

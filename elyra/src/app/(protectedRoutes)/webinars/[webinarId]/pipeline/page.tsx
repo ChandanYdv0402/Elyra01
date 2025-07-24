82c19299c48c4518528e78633501420f24ddd5de
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 18:54:52 +0530

    chore(page): scaffold pipeline attendance page

---
+++ elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const page = async ({ params }: { params: Promise<{ webinarId: string }> }) => {
+  return <div>Loading pipeline attendance...</div>;
+};
+
+export default page;

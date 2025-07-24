65514da46414f9f35604c6c46ae59f6d7b951b01
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 19:10:37 +0530

    feat(data): integrate getWebinarAttendance

--- elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import React from "react";
-import { onAuthenticateUser } from "@/action/auth";
-import { redirect } from "next/navigation";
+import { onAuthenticateUser }     from "@/action/auth";
+import { redirect }               from "next/navigation";
+import { getWebinarAttendance }   from "@/action/attendance";
 
- const page = async ({ params }) => {
- const checkUser = await onAuthenticateUser();
- if (!checkUser.user) {
-   redirect("/sign-in");
- }
+type Props = {
+  params: Promise<{
+    webinarId: string;
+  }>;
+};
 
-   return <div>Loading pipeline attendance...</div>;
- };
+const page = async ({ params }: Props) => {
+  const { webinarId } = await params;
+  const pipelineData = await getWebinarAttendance(webinarId);
 
+  const checkUser = await onAuthenticateUser();
+  if (!checkUser.user) {
+    redirect("/sign-in");
+  }
 
-const page = async ({ params }: { params: Promise<{ webinarId: string }> }) => {
   return <div>Loading pipeline attendance...</div>;
 };
 

5c8d3c7eb666b0c6200bde3de63f095e6d1b5fa2
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 18:58:08 +0530

    feat(auth): add onAuthenticateUser guard with redirect to sign‑in

--- elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
@@ -1,5 +1,18 @@
 "use client";
+
 import React from "react";
+import { onAuthenticateUser } from "@/action/auth";
+import { redirect } from "next/navigation";
+
+ const page = async ({ params }) => {
+ const checkUser = await onAuthenticateUser();
+ if (!checkUser.user) {
+   redirect("/sign-in");
+ }
+
+   return <div>Loading pipeline attendance...</div>;
+ };
+
 
 const page = async ({ params }: { params: Promise<{ webinarId: string }> }) => {
   return <div>Loading pipeline attendance...</div>;

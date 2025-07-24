fe7b494f7ba19d179143114d39de0ee40c8a9499
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-07-24 19:11:51 +0530

    feat(ui): show “No Pipelines Found” if no data

--- elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
+++ elyra/src/app/(protectedRoutes)/webinars/[webinarId]/pipeline/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
-import { onAuthenticateUser }     from "@/action/auth";
-import { redirect }               from "next/navigation";
-import { getWebinarAttendance }   from "@/action/attendance";
+import { onAuthenticateUser }   from "@/action/auth";
+import { redirect }             from "next/navigation";
+import { getWebinarAttendance } from "@/action/attendance";
 
 type Props = {
   params: Promise<{
@@ -14,6 +14,14 @@ type Props = {
 const page = async ({ params }: Props) => {
   const { webinarId } = await params;
   const pipelineData = await getWebinarAttendance(webinarId);
+
+  if (!pipelineData.data) {
+    return (
+      <div className="text-3xl h-[400px] flex justify-center items-center">
+        No Pipelines Found
+      </div>
+    );
+  }
 
   const checkUser = await onAuthenticateUser();
   if (!checkUser.user) {

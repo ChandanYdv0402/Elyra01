8a7bad97642278d69f6c21320db8bfd8acb4d470
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-07 01:18:04 +0530

    feat(upcoming-state): add CountdownTimer and WaitListComponent

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import React from "react";
-import { Webinar, User } from "@prisma/client";
+import { Webinar, User, WebinarStatusEnum } from "@prisma/client";
+import CountdownTimer from "./CountdownTimer";
+import WaitListComponent from "./WaitListComponent";
 
 type Props = {
   webinar: Webinar;
@@ -9,7 +11,31 @@ type Props = {
 };
 
 const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
-  return <div>WebinarUpcomingState</div>;
+  if (webinar.webinarStatus === WebinarStatusEnum.ENDED) {
+    return <div>Webinar ended</div>;
+  }
+
+  return (
+    <div>
+      <CountdownTimer
+        targetDate={new Date(webinar.startTime)}
+        webinarId={webinar.id}
+        webinarStatus={webinar.webinarStatus}
+      />
+      {webinar.webinarStatus === WebinarStatusEnum.SCHEDULED && (
+        <WaitListComponent
+          webinarId={webinar.id}
+          webinarStatus="SCHEDULED"
+        />
+      )}
+      {webinar.webinarStatus === WebinarStatusEnum.WAITING_ROOM && (
+        <WaitListComponent
+          webinarId={webinar.id}
+          webinarStatus="WAITING_ROOM"
+        />
+      )}
+    </div>
+  );
 };
 
 export default WebinarUpcomingState;

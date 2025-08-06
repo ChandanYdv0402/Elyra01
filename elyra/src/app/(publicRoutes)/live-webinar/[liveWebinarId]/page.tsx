9958d225b79d1b601b859c6cc7bdd7b5d5aeaa62
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-07 00:04:16 +0530

    chore(page): scaffold page and fetch webinar data

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/page.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { getWebinarById } from '@/action/webinar'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+type Props = {
+  params: Promise<{ liveWebinarId: string }>
+  searchParams: Promise<{ error: string }>
+}
+
+const page = async ({ params, searchParams }: Props) => {
+  const { liveWebinarId } = await params
+  const { error } = await searchParams
+
+  const webinarData = await getWebinarById(liveWebinarId)
+  if (!webinarData) {
+    return (
+      <div className="w-full min-h-screen flex justify-center items-center text-lg sm:text-4xl">
+        Webinar not found
+      </div>
+    )
+  }
+
+  return <div>Webinar loaded</div>
 }
 
 export default page

36b404af21ea374b0f6b633976d84e4a1b568aa3
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-07 01:19:22 +0530

    feat(upcoming-state): full UI with thumbnail, countdown, start button & date/time**

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { User, Webinar, WebinarStatusEnum } from "@prisma/client";
-import { Loader2 } from "lucide-react";
-import CountdownTimer from "./CountdownTimer";
+import { format } from "date-fns";
+import { Calendar, Clock, Loader2 } from "lucide-react";
+import React, { useState } from "react";
+import Image from "next/image";
 import WaitListComponent from "./WaitListComponent";
+import CountdownTimer from "./CountdownTimer";
+import { toast } from "sonner";
 import { changeWebinarStatus } from "@/action/webinar";
+import { useRouter } from "next/navigation";
 import { sendBulkEmail } from "@/action/resend";
 import { createAndStartStream } from "@/action/stremIo";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 
 type Props = {
   webinar: Webinar;
@@ -45,30 +47,94 @@ const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
     }
   };
 
-  if (
-    webinar.webinarStatus === WebinarStatusEnum.WAITING_ROOM &&
-    currentUser?.id === webinar.presenterId
-  ) {
-    return (
-      <Button onClick={handleStartWebinar} disabled={loading}>
-        {loading ? (
-          <>
-            <Loader2 className="animate-spin mr-2" />
-            Starting...
-          </>
-        ) : (
-          "Start Webinar"
-        )}
-      </Button>
-    );
-  }
-
   return (
-    <CountdownTimer
-      targetDate={new Date(webinar.startTime)}
-      webinarId={webinar.id}
-      webinarStatus={webinar.webinarStatus}
-    />
+    <div className="w-full min-h-screen mx-auto max-w-[400px] flex flex-col justify-center items-center gap-8 py-20">
+      <div className="space-y-6">
+        <p className="text-3xl font-semibold text-primary text-center">
+          Seems Like you are a little early
+        </p>
+        <CountdownTimer
+          targetDate={new Date(webinar.startTime)}
+          className="text-center"
+          webinarId={webinar.id}
+          webinarStatus={webinar.webinarStatus}
+        />
+      </div>
+
+      <div className="space-y-6 w-full h-full flex justify-center items-center flex-col">
+        <div className="w-full max-w-md aspect-video relative rounded-4xl overflow-hidden mb-6">
+          <Image
+            src="/ilfthumbnail.png"
+            alt={webinar.title}
+            fill
+            className="object-cover"
+            priority
+          />
+        </div>
+
+        {webinar.webinarStatus === WebinarStatusEnum.LIVE ? (
+          <WaitListComponent webinarId={webinar.id} webinarStatus="LIVE" />
+        ) : webinar.webinarStatus === WebinarStatusEnum.CANCELLED ? (
+          <p className="text-xl text-foreground text-center font-semibold">
+            Webinar is cancelled
+          </p>
+        ) : webinar.webinarStatus === WebinarStatusEnum.ENDED ? (
+          <Button disabled>Ended</Button>
+        ) : webinar.webinarStatus === WebinarStatusEnum.SCHEDULED &&
+          new Date(webinar.startTime).getTime() > Date.now() ? (
+          <WaitListComponent
+            webinarId={webinar.id}
+            webinarStatus="SCHEDULED"
+          />
+        ) : webinar.webinarStatus === WebinarStatusEnum.WAITING_ROOM ||
+          new Date(webinar.startTime).getTime() < Date.now() ? (
+          currentUser?.id === webinar.presenterId ? (
+            <Button
+              className="w-full max-w-[300px] font-semibold"
+              onClick={handleStartWebinar}
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Loader2 className="animate-spin mr-2" />
+                  Starting...
+                </>
+              ) : (
+                "Start Webinar"
+              )}
+            </Button>
+          ) : (
+            <WaitListComponent
+              webinarId={webinar.id}
+              webinarStatus="WAITING_ROOM"
+            />
+          )
+        ) : null}
+      </div>
+
+      <div className="text-center space-y-4">
+        <h3 className="text-2xl font-semibold text-primary">
+          {webinar.title}
+        </h3>
+        <p className="text-muted-foreground text-xs">
+          {webinar.description}
+        </p>
+        <div className="w-full justify-center flex gap-2 flex-wrap items-center">
+          <Button
+            variant="outline"
+            className="rounded-md bg-secondary backdrop-blur-2xl"
+          >
+            <Calendar className="mr-2" />
+            {format(new Date(webinar.startTime), "dd MMMM yyyy")}
+          </Button>
+
+          <Button variant="outline">
+            <Clock className="mr-2" />
+            {format(new Date(webinar.startTime), "hh:mm a")}
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 

8e5bcdb968d70cf85cd77ac7f1341f0db118f130
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-07 01:20:08 +0530

    fix: some import errors

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/UpcomingWebinar/WebinarUpcomingState.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { User, Webinar, WebinarStatusEnum } from "@prisma/client";
 import { format } from "date-fns";
@@ -14,22 +12,23 @@ import { useRouter } from "next/navigation";
 import { sendBulkEmail } from "@/action/resend";
 import { createAndStartStream } from "@/action/stremIo";
 
+
 type Props = {
   webinar: Webinar;
   currentUser: User | null;
 };
 
 const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
+
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-
   const handleStartWebinar = async () => {
     setLoading(true);
     try {
       if (!currentUser?.id) {
         throw new Error("User not authenticated");
       }
-      await createAndStartStream(webinar);
+      await createAndStartStream(webinar)
       const res = await changeWebinarStatus(webinar.id, "LIVE");
       if (!res.success) {
         throw new Error(res.message);
@@ -39,20 +38,19 @@ const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
       toast.success("Webinar started successfully");
     } catch (error) {
       console.log(error);
-      toast.error(
-        error instanceof Error ? error.message : "Something went wrong"
-      );
+      toast.error(error instanceof Error ? error.message : "Something went wrong");
     } finally {
       setLoading(false);
     }
   };
-
   return (
     <div className="w-full min-h-screen mx-auto max-w-[400px] flex flex-col justify-center items-center gap-8 py-20">
+      {/* timer container */}
       <div className="space-y-6">
         <p className="text-3xl font-semibold text-primary text-center">
           Seems Like you are a little early
         </p>
+
         <CountdownTimer
           targetDate={new Date(webinar.startTime)}
           className="text-center"
@@ -61,34 +59,31 @@ const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
         />
       </div>
 
+      {/* Reminder component */}
       <div className="space-y-6 w-full h-full flex justify-center items-center flex-col">
         <div className="w-full max-w-md aspect-video relative rounded-4xl overflow-hidden mb-6">
           <Image
-            src="/ilfthumbnail.png"
+            src={"/ilfthumbnail.png"}
             alt={webinar.title}
             fill
             className="object-cover"
             priority
           />
         </div>
-
-        {webinar.webinarStatus === WebinarStatusEnum.LIVE ? (
+        {webinar?.webinarStatus === WebinarStatusEnum.LIVE ? (
           <WaitListComponent webinarId={webinar.id} webinarStatus="LIVE" />
-        ) : webinar.webinarStatus === WebinarStatusEnum.CANCELLED ? (
+        ) : webinar?.webinarStatus === WebinarStatusEnum.CANCELLED ? (
           <p className="text-xl text-foreground text-center font-semibold">
             Webinar is cancelled
           </p>
-        ) : webinar.webinarStatus === WebinarStatusEnum.ENDED ? (
-          <Button disabled>Ended</Button>
-        ) : webinar.webinarStatus === WebinarStatusEnum.SCHEDULED &&
+        ) : webinar?.webinarStatus === WebinarStatusEnum.ENDED ? (
+          <Button disabled={true}>Ended</Button>
+        ) : webinar?.webinarStatus === WebinarStatusEnum.SCHEDULED &&
           new Date(webinar.startTime).getTime() > Date.now() ? (
-          <WaitListComponent
-            webinarId={webinar.id}
-            webinarStatus="SCHEDULED"
-          />
-        ) : webinar.webinarStatus === WebinarStatusEnum.WAITING_ROOM ||
+          <WaitListComponent webinarId={webinar.id} webinarStatus="SCHEDULED" />
+        ) : webinar?.webinarStatus === WebinarStatusEnum.WAITING_ROOM ||
           new Date(webinar.startTime).getTime() < Date.now() ? (
-          currentUser?.id === webinar.presenterId ? (
+          currentUser?.id === webinar?.presenterId ? (
             <Button
               className="w-full max-w-[300px] font-semibold"
               onClick={handleStartWebinar}
@@ -112,23 +107,22 @@ const WebinarUpcomingState = ({ webinar, currentUser }: Props) => {
         ) : null}
       </div>
 
+      {/* description component */}
       <div className="text-center space-y-4">
         <h3 className="text-2xl font-semibold text-primary">
-          {webinar.title}
+          {webinar?.title}
         </h3>
-        <p className="text-muted-foreground text-xs">
-          {webinar.description}
-        </p>
+        <p className="text-muted-foreground text-xs">{webinar.description}</p>
         <div className="w-full justify-center flex gap-2 flex-wrap items-center">
           <Button
-            variant="outline"
+            variant={"outline"}
             className="rounded-md bg-secondary backdrop-blur-2xl"
           >
             <Calendar className="mr-2" />
             {format(new Date(webinar.startTime), "dd MMMM yyyy")}
           </Button>
 
-          <Button variant="outline">
+          <Button variant={"outline"}>
             <Clock className="mr-2" />
             {format(new Date(webinar.startTime), "hh:mm a")}
           </Button>

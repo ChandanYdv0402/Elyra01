9f968cb3cb2f34e3391afd81f7742bdb4ddb2195
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-07 00:06:20 +0530

    some final changes

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/page.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/page.tsx
@@ -1,21 +1,22 @@
 import { getWebinarById } from '@/action/webinar'
 import { onAuthenticateUser } from '@/action/auth'
-import { getStreamRecording } from '@/action/streamIo'
 import RenderWebinar from './_components/Common/RenderWebinar'
 import { WebinarWithPresenter } from '@/lib/type'
+import { getStreamRecording } from '@/action/stremIo'
 
 type Props = {
-  params: Promise<{ liveWebinarId: string }>
-  searchParams: Promise<{ error: string }>
+  params: Promise<{
+    liveWebinarId: string
+  }>
+  searchParams: Promise<{
+    error: string
+  }>
 }
 
 const page = async ({ params, searchParams }: Props) => {
   const { liveWebinarId } = await params
   const { error } = await searchParams
-
-  const checkUser = await onAuthenticateUser()
   const webinarData = await getWebinarById(liveWebinarId)
-
   if (!webinarData) {
     return (
       <div className="w-full min-h-screen flex justify-center items-center text-lg sm:text-4xl">
@@ -24,10 +25,12 @@ const page = async ({ params, searchParams }: Props) => {
     )
   }
 
+  const checkUser = await onAuthenticateUser()
   const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY as string
   let recording = null
   if (webinarData.webinarStatus === 'ENDED') {
     recording = await getStreamRecording(webinarData.id)
+    console.log(recording)
   }
 
   return (

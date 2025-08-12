be7b1b6f8eb7881fd5e6a5efd25464d74690577d
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-13 02:34:38 +0530

    feat(livestream): fetch host token and init StreamVideoClient
    tsx
    Copy
    Edit

--- elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/LiveWebinar/LiveStreamState.tsx
+++ elyra/src/app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/LiveWebinar/LiveStreamState.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useState } from "react";
-import { StreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+import {
+  StreamVideoClient,
+  User as StreamUser,
+} from "@stream-io/video-react-sdk";
 import { User } from "@prisma/client";
 import { WebinarWithPresenter } from "@/lib/type";
+import { getTokenForHost } from "@/action/stremIo";
 
 type Props = {
   apiKey: string;
@@ -15,6 +19,38 @@ type Props = {
 const LiveStreamState = ({ apiKey, callId, webinar, user }: Props) => {
   const [hostToken, setHostToken] = useState<string | null>(null);
   const [client, setClient] = useState<StreamVideoClient | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const token = await getTokenForHost(
+          webinar.presenterId,
+          webinar.presenter.name,
+          webinar.presenter.profileImage
+        );
+
+        const hostUser: StreamUser = {
+          id: webinar.presenterId,
+          name: webinar.presenter.name,
+          image: webinar.presenter.profileImage,
+        };
+
+        const streamClient = new StreamVideoClient({
+          apiKey,
+          user: hostUser,
+          token,
+        });
+
+        setHostToken(token);
+        setClient(streamClient);
+      } catch (error) {
+        console.error("Error initializing stream client", error);
+      }
+    };
+
+    init();
+  }, [apiKey, webinar]);
+
   return null;
 };
 

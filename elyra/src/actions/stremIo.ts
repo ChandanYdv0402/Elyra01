8606257ef32e75ffb81d78bc22cc98dfc1c4e554
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-08-13 16:18:47 +0530

    feat(stream): some final changes

--- elyra/src/actions/stremIo.ts
+++ elyra/src/actions/stremIo.ts
@@ -31,27 +31,31 @@ export const getStreamIoToken = async (attendee: Attendee | null) => {
 export const createAndStartStream = async (webinar: Webinar) => {
   try {
     const checkWebinar = await prismaClient.webinar.findMany({
-      where: {
+      where:{
         presenterId: webinar.presenterId,
-        webinarStatus: "LIVE",
-      },
-    });
+        webinarStatus:"LIVE"
+      }
+    })
 
-    if (checkWebinar.length > 0) {
-      throw new Error("You already have a live stream running");
+    if(checkWebinar.length > 0) {
+      throw new Error("You already have a live stream running")
     }
-
     const call = getStreamClient.video.call("livestream", webinar.id);
     await call.getOrCreate({
       data: {
         // starts_at: new Date(webinar.startTime),
         created_by_id: webinar.presenterId,
-        members: [{ user_id: webinar.presenterId, role: "host" }],
+        members: [
+          {
+            user_id: webinar.presenterId,
+            role: "host",
+          },
+        ],
       },
     });
-    await call.goLive({
+    call.goLive({
       start_recording: true,
-      recording_storage_name: "livestream",
+      recording_storage_name:"livestream",
     });
     console.log("Stream started successfully:", call);
   } catch (error) {
@@ -70,7 +74,9 @@ export const getTokenForHost = async (
       id: userId,
       role: "user",
       name: username || "Guest",
-      image: profilePic || `https://api.dicebear.com/7.x/initials/svg?seed=${username}`,
+      image:
+        profilePic ||
+        `https://api.dicebear.com/7.x/initials/svg?seed=${username}`,
     };
     await getStreamClient.upsertUsers([newUser]);
 
@@ -87,22 +93,25 @@ export const getTokenForHost = async (
   }
 };
 
-export const getStreamRecording = async (webinarId: string) => {
-  try {
+
+//get call recording
+export const getStreamRecording = async(webinarId:string) =>{
+  try{
     const call = getStreamClient.video.call("livestream", webinarId);
-    const calls = await call.listRecordings();
+
+    const calls = await call.listRecordings()
 
     return {
       success: true,
       status: 200,
-      data: calls.recordings[calls.recordings.length - 1],
-    };
-  } catch (error) {
+      data: calls.recordings[calls.recordings.length-1],
+    }
+  }catch(error){
     console.error("Error generating Stream Io token:", error);
     return {
       success: false,
       status: 500,
       message: "Failed to generate Stream Io token",
-    };
+    }
   }
-};
+}
